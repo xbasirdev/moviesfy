94ed54f5792119751b9ab15a0a10fef3e2eef7fb
Xavier Basir <xavierfiv@gmail.com>
2020-02-01 12:47:31 -0400

    fix: ajuste a migracion de peliculas

--- database/migrations/2019_09_23_132046_create_files_table.php
+++ database/migrations/2019_09_23_132046_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMoviesTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateMoviesTable extends Migration
      */
     public function up()
     {
-        Schema::create('movies', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('description');
-            $table->string('genre');
-            $table->integer('year');
-            $table->string('duration');
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('filename');
+            $table->bigInteger('user_id')->unsigned()->index();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateMoviesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movies');
+        Schema::dropIfExists('files');
     }
 }
